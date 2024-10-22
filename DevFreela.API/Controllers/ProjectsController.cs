74f67daae3bdef427bc1f039c21718cf1e3239ca
Daniel Ferreira <daniel.alexandre22@hotmail.com>
2024-10-22 11:04:51 -0300

    Projeto iniciado

---
+++ DevFreela.API/Controllers/ProjectsController.cs
@@ -0,0 +1,72 @@
+﻿using DevFreela.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace DevFreela.API.Controllers
+{
+    [Route("api/projects")]
+    [ApiController]
+    public class ProjectsController : ControllerBase
+    {
+
+        public readonly FreelanceTotalCostConfig _config;
+
+        public ProjectsController(IOptions<FreelanceTotalCostConfig> options)
+        {
+            _config = options.Value;
+        }
+
+        [HttpGet]
+        public IActionResult Get(string search)
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Post(CreateProjectInputModel model)
+        {
+            if (model.TotalCost < _config.Minimum || model.TotalCost > _config.Maximum)
+            {
+                return BadRequest("Número fora dos limites");
+            }
+            return CreatedAtAction(nameof(GetById), new { id = 1 }, model);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(UpdateProjectInputModel model)
+        {
+            return NoContent();
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            return NoContent();
+        }
+
+        [HttpPut("{id}/start")]
+        public IActionResult Start(int id)
+        {
+            return NoContent();
+        }
+
+        [HttpPut("{id}/Complete")]
+        public IActionResult Complete(int id)
+        {
+            return NoContent();
+        }
+
+        [HttpPost("{id}/comments")]
+        public IActionResult PostComment(int id, CreateProjecCommentInputModel model)
+        {
+            return Ok();
+        }
+
+    }
+}
---
+++ DevFreela.API/Models/CreateProjecCommentInputModel.cs
@@ -0,0 +1,9 @@
+﻿namespace DevFreela.API.Models
+{
+    public class CreateProjecCommentInputModel
+    {
+        public string Content { get; set; }
+        public int IdProject { get; set; }
+        public int IdUser { get; set; }
+    }
+}
